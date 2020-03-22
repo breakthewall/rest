54fef7f859034ec3e3819e216d4a88afc9f68949
joan <joan.herisson@univ-evry.fr>
2020-03-22 01:42:27 +0100

    formatting

--- app/default/test/rest-query.sh
+++ app/default/test/rest-query.sh
@@ -2,4 +2,8 @@
 
 pip install --upgrade pip
 pip install requests
-echo -e "import requests\nr = requests.post('http://flask:8000/default', 'http://nvie.com')\nprint(r.content)" | python3
+echo -e "
+import requests
+r = requests.post('http://flask:8000/default', 'http://nvie.com')
+print(r.content)
+" | python3
