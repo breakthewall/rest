421dc8d8632ce69c70616e84d098fc6c0162a394
joan <joan.herisson@univ-evry.fr>
2020-03-22 02:07:34 +0100

    import

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/<tool>', methods=['POST'])
 def main(tool):
 
-    tool_module = import_module("."+tool, tool)
+    tool_module = import_module("."+tool, tool+".rest")
 
     if request.method == "POST":
 
