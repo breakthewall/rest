58e3e4d59738b4581ca5df22525de94dd18012e2
joan <joan.herisson@univ-evry.fr>
2020-03-23 17:02:49 +0100

    network

--- bin/start-flask.sh
+++ bin/start-flask.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 docker-compose -f docker-compose-flask.yml up -d
-docker network connect galaxy "$(docker inspect -f '{{.Name}}' $(docker-compose -f docker-compose-flask.yml ps -q)  | cut -c2-)"
+docker network connect \
+  --alias flask \
+  galaxy \
+  "$(docker inspect -f '{{.Name}}' $(docker-compose -f docker-compose-flask.yml ps -q)  | cut -c2-)" \
--- bin/stop-flask.sh
+++ bin/stop-flask.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+echo docker network disconnect galaxy "$(docker inspect -f '{{.Name}}' $(docker-compose -f docker-compose-flask.yml ps -q)  | cut -c2-)"
 docker-compose -f docker-compose-flask.yml down
