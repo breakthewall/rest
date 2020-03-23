6bd71362a33cb9d273289dcef3aa76aa5d5880cf
joan <joan.herisson@univ-evry.fr>
2020-03-23 16:15:22 +0100

    network disconnect

--- bin/stop-worker.sh
+++ bin/stop-worker.sh
@@ -10,4 +10,5 @@ if [[ "$app" == "" ]]; then
   exit 1
 fi
 
+docker network disconnect $app "$(docker inspect -f '{{.Name}}' $(docker-compose -f docker-compose-flask.yml ps -q)  | cut -c2-)"
 COMPOSE_PROJECT_NAME=$app docker-compose -f docker-compose-worker.yml down
